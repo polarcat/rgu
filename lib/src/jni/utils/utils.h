a77621962123760a6dfc23d3535e7c9d9e8b2a5c
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2018-10-27 12:47:44 +0300

    utils: common utils header initial commit

---
+++ lib/src/jni/utils/utils.h
@@ -0,0 +1,12 @@
+/* utils.h: common utils
+ *
+ * Copyright (c) 2018, Aliaksei Katovich <aliaksei.katovich at gmail.com>
+ *
+ * Released under the GNU General Public License, version 2
+ */
+
+#pragma once
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+#endif
