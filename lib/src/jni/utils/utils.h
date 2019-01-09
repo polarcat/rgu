00ee6832c8eaf54b65c13fdafa1beba685148b00
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2019-01-09 22:52:44 +0200

    utils: add container of macro

--- lib/src/jni/utils/utils.h
+++ lib/src/jni/utils/utils.h
@@ -10,3 +10,8 @@
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 #endif
+
+#define container_of(ptr, type, member) __extension__ ({\
+	const __typeof__(((type *) 0)->member) * __mptr = (ptr);\
+	(type *) ((char *) __mptr - offsetof(type, member));\
+})
