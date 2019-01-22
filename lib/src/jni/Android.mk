7f9a9f4ab7c317e8386a5a66b0487f09cafaf01d
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2019-01-22 21:47:18 +0200

    jni: compile sensors and img modules

--- lib/src/jni/Android.mk
+++ lib/src/jni/Android.mk
@@ -8,12 +8,14 @@ lib3d/jni.c \
 lib3d/gl.c \
 lib3d/gm.c \
 lib3d/bg.c \
+lib3d/img.c \
 lib3d/font.c \
 lib3d/draw.c \
 lib3d/pip.c \
 lib3d/cv.c \
 lib3d/plotter.c \
 utils/image.c \
+utils/sensors.c \
 
 ifneq (,$(wildcard $(LOCAL_PATH)/algo))
 LOCAL_CFLAGS += "-DHAVE_ALGO"
@@ -30,6 +32,6 @@ LOCAL_C_INCLUDES += $(PNG_INC) $(JPEG_INC)
 LOCAL_LDLIBS += -lm -llog -lGLESv2
 LOCAL_LDLIBS += -landroid # assets handling
 LOCAL_LDLIBS += -Wl,--start-group $(PNG_LIB) $(JPEG_LIB) -Wl,--end-group
-LOCAL_CFLAGS += "-DLIB_TAG=\"lib3d\""
+LOCAL_CFLAGS += "-DLIB_TAG=\"lib3d\"" -DIMAGE_VIEWER
 
 include $(BUILD_SHARED_LIBRARY)
