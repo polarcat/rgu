bcbf08862a43db94961404225c77532b6467d90f
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2019-03-14 23:43:27 +0200

    utils: add sem_post_checked

--- lib/src/jni/utils/utils.h
+++ lib/src/jni/utils/utils.h
@@ -8,12 +8,25 @@
 #pragma once
 
 #include <stddef.h>
+#include <semaphore.h>
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 #endif
 
+#ifndef container_of
 #define container_of(ptr, type, member) __extension__ ({\
 	const __typeof__(((type *) 0)->member) * __mptr = (ptr);\
 	(type *) ((char *) __mptr - offsetof(type, member));\
 })
+#endif
+
+static inline void sem_post_checked(sem_t *sem)
+{
+	int val = 1; /* to skip error checking */
+
+	sem_getvalue(sem, &val);
+
+	if (!val)
+		sem_post(sem);
+}
